08a49f45b62da19d81900e628414397541a7a207
lilohdong <rhkdrh102010@gmail.com>
2025-11-22 00:09:43 +0900

    필요없어서 지웠습니다

--- src/util/Sizes.java
+++ src/util/Sizes.java
@@ -8,11 +8,5 @@ public class Sizes {
     // 사이드바 사이즈
     public static final int SIDEBAR_WIDTH = 256;
     public static final int SIDEBAR_HEIGHT = 832;
-    // Game 관련 패널 사이즈 - 동준
-    public static final int GAME_MAIN_STATISTICS_WIDTH = 737;
-    public static final int GAME_MAIN_STATISTICS_HEIGHT = 758;
-
-    public static final int GAME_HEADER_HEIGHT = 74;
-
     public static final int GAME_POPULAR_WIDTH = 269;
 }
