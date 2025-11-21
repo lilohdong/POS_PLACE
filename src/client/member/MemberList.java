d33de20ce69189d22cbee002e52fc095ca482980
leewonno <leeone040119@gmail.com>
2025-11-21 16:13:32 +0900

    수정 ing.

--- src/client/member/MemberList.java
+++ src/client/member/MemberList.java
@@ -6,82 +6,107 @@ import java.awt.event.*;
 
 class Member extends JFrame implements ActionListener{
 
-    public Member(Memberbtns mbtns){
+    public Member(Memberbtns mbtns,SearchMember smem ){
+
+        setLayout( new BorderLayout());
+
         add(mbtns,BorderLayout.NORTH);
+        add(smem,BorderLayout.CENTER);
     }
 
-    class Memberbtns extends JPanel implements ActionListener{
+    @Override
+    public void actionPerformed(ActionEvent e){
 
-        JButton btnJoin, btnUpdate, btnDelete;
+    }
+}
 
-        Memberbtns(){
-            setLayout(new FlowLayout(FlowLayout.LEFT));
+class Memberbtns extends JPanel implements ActionListener{
 
-            btnJoin = new JButton("가입");
-            btnUpdate = new JButton("수정");
-            btnDelete = new JButton("삭제");
+    JButton btnJoin, btnUpdate, btnDelete;
 
-            add(btnJoin);
-            add(btnUpdate);
-            add(btnDelete);
+    Memberbtns(){
+        setLayout(new FlowLayout(FlowLayout.LEFT));
 
-            btnJoin.addActionListener(this);
-            btnUpdate.addActionListener(this);
-            btnDelete.addActionListener(this);
+        btnJoin = new JButton("가입");
+        btnUpdate = new JButton("수정");
+        btnDelete = new JButton("삭제");
 
-        }
+        add(btnJoin);
+        add(btnUpdate);
+        add(btnDelete);
 
-        @Override
-        public void  actionPerformed(ActionEvent e){
+        btnJoin.addActionListener(this);
+        btnUpdate.addActionListener(this);
+        btnDelete.addActionListener(this);
 
-        }
     }
 
-    class SearchMember extends JPanel implements ActionListener{
+    @Override
+    public void  actionPerformed(ActionEvent e){
 
-        JComboBox<String> combo;
-        private String[] searchMethod = {"전체검색", "이름" , "아이디"};
-
-        JTextField searchField;
-        JButton searchBtn;
-
-        SearchMember(){
-            setLayout( new FlowLayout(FlowLayout.LEFT));
-
-            combo = new JComboBox<>(searchMethod);
-            searchField = new JTextField();
-            searchBtn = new JButton();
-
-            add(combo);
-            add(searchField);
-            add(searchBtn);
-
-            searchBtn.addActionListener(this);
-        }
-
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-
-        }
     }
+}
 
+class SearchMember extends JPanel implements ActionListener{
+
+    JComboBox<String> combo;
+    private String[] searchMethod = {"전체검색", "이름" , "아이디"};
+
+    JTextField searchField;
+    JButton searchBtn;
+
+    SearchMember(){
+        setLayout( new FlowLayout(FlowLayout.LEFT));
+
+        combo = new JComboBox<>(searchMethod);
+        searchField = new JTextField(30);
+        searchBtn = new JButton();
+
+
+        add(combo);
+        add(searchField);
+        add(searchBtn);
+
+        searchBtn.addActionListener(this);
+    }
 
 
     @Override
-    public void actionPerformed(ActionEvent e){}
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
+
+class MTable extends JPanel implements ActionListener{
+
+    JTable table;
+    JScrollPane scroll;
+
+    MTable(){
+
+        setLayout( new BorderLayout());
+
+    }
+
+    @Override
+    public void  actionPerformed(ActionEvent e){
+
+    }
 }
 
 
 public class MemberList {
     public static void main(String[] args){
-        Member member = new Member(mbtns);
-        Member.Memberbtns mbtns = member.new Memberbtns();
-        Member.SearchMember smem = member.new SearchMember();
+        Memberbtns mbtns = new Memberbtns();
+        SearchMember smem = new SearchMember();
+        MTable mtable = new MTable();
+        Member member = new Member(mbtns,smem);
+
 
         member.setTitle("회원 관리");
         member.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         member.setSize(1200,832);
         member.setVisible(true);
     }
+
 }
